4ab110d8d2ef2ade4a62eaf18397adbb3826906c
Arklashh <schollin.alexander@gmail.com>
2023-08-16 14:44:28 +0200

    Drawing

---
+++ Assets/Scripts/LineGenerator.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineGenerator : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    Line activeLine;
+
+    public Camera camera;
+
+    public float zDistance = 10f;
+    public float zOffset = 10f;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject newLine = Instantiate(linePrefab);
+            activeLine = newLine.GetComponent<Line>();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePosA = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, zDistance);
+            Vector3 mousePos = camera.ScreenToWorldPoint(mousePosA);
+            activeLine.UpdateLine(mousePos);
+            Debug.Log("UpdateLine " + mousePos + " " + mousePosA);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            activeLine = null;
+        }
+
+        // if (activeLine != null)
+        // {
+        //     Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //     activeLine.UpdateLine(mousePos);
+        // }
+    }
+}
